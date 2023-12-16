b717636f608b21aa185472cb5f8856f779f8ce3b
tihaa29 <129364313+tihaa29@users.noreply.github.com>
2023-12-16 09:09:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 // Music
-const musicSound = new Audio('audio/hapi.mp3');
+const musicSound = new Audio('hapi.mp3');
 musicSound.loop = true;
 musicSound.play();
 
 if(!musicSound.paused){
     var musicBtn = document.getElementById('music');
-    musicBtn.style.background = "url('images/musicBtn.jpg') no-repeat";
+    musicBtn.style.background = "url('musicBtn.jpg') no-repeat";
     musicBtn.style.backgroundSize = "cover";
     musicBtn.style.backgroundPosition = "center";
 }
 
-var clickAudio = new Audio('audio/click.wav');
+var clickAudio = new Audio('click.wav');
 let isSoundOn = true;
 clickAudio.volume = 1;
 
@@ -27,13 +27,13 @@ function optionClicked(id) {
         if(!isSoundOn){
             clickAudio.volume = 1;
             isSoundOn = true;
-            soundBtn.style.background = "url('images/voiceBtn.jpg')";
+            soundBtn.style.background = "url('voiceBtn.jpg')";
             soundBtn.style.backgroundSize = "cover";
             soundBtn.style.backgroundPosition = "center";
         } else {
             clickAudio.volume = 0;
             isSoundOn = false;
-            soundBtn.style.background = "url('images/voiceMuteBtn.png')";
+            soundBtn.style.background = "url('voiceMuteBtn.png')";
             soundBtn.style.backgroundSize = "cover";
             soundBtn.style.backgroundPosition = "center";
         }
@@ -43,12 +43,12 @@ function optionClicked(id) {
         // Toggle playback
         if (musicSound.paused) {
             musicSound.play();
-            musicBtn.style.background = "url('images/musicBtn.jpg') no-repeat";
+            musicBtn.style.background = "url('musicBtn.jpg') no-repeat";
             musicBtn.style.backgroundSize = "cover";
             musicBtn.style.backgroundPosition = "center";
         } else {
             musicSound.pause();
-            musicBtn.style.background = "url('images/musicMuteBtn.png') no-repeat";
+            musicBtn.style.background = "url('musicMuteBtn.png') no-repeat";
             musicBtn.style.backgroundSize = "cover";
             musicBtn.style.backgroundPosition = "center";
         }
